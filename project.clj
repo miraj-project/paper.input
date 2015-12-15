fc913457fd7d2df409f5dd4b7abfbe5464a60f99
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-15 08:40:30 -0600

    fix proj description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject polymer/paper.input "1.2.3-SNAPSHOT"
-  :description "Polymer Paper markup functions"
+  :description "Polymer Paper Input bundled element markup functions"
   :url "https://github.com/mobileink/polymer.paper.input"
   :license {:name "EPL"
             :url "http://opensource.org/licenses/eclipse-1.0.php"}
