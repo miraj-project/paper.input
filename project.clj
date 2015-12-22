f793cc6b070de09c08fac1471be5f9723010c11b
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-22 08:57:17 -0600

    update project.clj, license

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :url "https://github.com/mobileink/polymer.paper.input"
   :license {:name "EPL"
             :url "http://opensource.org/licenses/eclipse-1.0.php"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
-                 [miraj/markup "0.1.0-SNAPSHOT"]
-                 [polymer/elements.paper "1.2.3-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+                 [miraj/markup "0.1.0-SNAPSHOT"]])
