b97a61e3ccfb017164e20b38ae0fee87500a280f
Gregg Reynolds <reynolds-gregg@norc.org>
2017-01-04 13:02:33 -0600

    checkpoint

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject polymer/paper.input "1.2.3-SNAPSHOT"
+(defproject miraj/paper.input "1.2.3-SNAPSHOT"
   :description "Polymer Paper Input bundled element markup functions"
   :url "https://github.com/mobileink/polymer.paper.input"
   :license {:name "EPL"
             :url "http://opensource.org/licenses/eclipse-1.0.php"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [miraj/markup "0.1.0-SNAPSHOT"]])
